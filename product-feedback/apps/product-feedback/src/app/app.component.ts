3decc766eb29015c8e44f7fe364a31edbfa24d5c
Deion Etman <deion@newstory.nl>
2024-03-20 17:22:31 +0100

    Made small changes to the imports to resolve merge conflict

--- product-feedback/apps/product-feedback/src/app/app.component.ts
+++ product-feedback/apps/product-feedback/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { SuggestionCardComponent } from '@components/suggestion-card/suggestion-card.component';
 import { NoSuggestionsComponent } from '@components/no-suggestions/no-suggestions.component';
 import { HomePageComponent } from '@components/HomePage/HomePage.component';
 @Component({
   standalone: true,
-  imports: [
-    RouterModule,
-    TestComponent,
-    HomePageComponent,
-    SuggestionCardComponent,
-    NoSuggestionsComponent,
-  ],
+  imports: [HomePageComponent, SuggestionCardComponent, NoSuggestionsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
