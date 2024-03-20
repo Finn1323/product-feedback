3817f8111d86173684503f04300ca8c61ebe2a03
Finn <finn@newstory.nl>
2024-03-20 16:36:55 +0100

    Added Category and Roadmap component

---
+++ product-feedback/libs/src/lib/components/HomePage/Roadmap.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-roadmap',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './Roadmap.component.html',
+  styleUrl: './Roadmap.component.css',
+})
+export class RoadmapComponent {}
