89e4d0f3859479a2196cbfd9b26be56bc1527633
Deion Etman <deion@newstory.nl>
2024-03-22 10:03:10 +0100

    fixed merge coflicts

--- product-feedback/apps/product-feedback/src/app/pages/HomePage.component.ts
+++ product-feedback/apps/product-feedback/src/app/pages/HomePage.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CategoryComponent } from './Category.component';
-import { RoadmapComponent } from './Roadmap.component';
+import { CategoryComponent } from '@components/category/Category.component';
+import { RoadmapComponent } from '@components/roadmap/Roadmap.component';
 @Component({
-  selector: 'lib-home-page',
+  selector: 'app-home-page',
   standalone: true,
   imports: [CommonModule, CategoryComponent, RoadmapComponent],
   templateUrl: './HomePage.component.html',
