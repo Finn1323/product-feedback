2391d1c32411ac29ac2dc7a51d31ad2c53b10dc7
Finn <finn@newstory.nl>
2024-03-22 09:49:30 +0100

    changed folder structure

--- product-feedback/apps/product-feedback/src/app/app.component.ts
+++ product-feedback/apps/product-feedback/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { SuggestionCardComponent } from '@components/suggestion-card/suggestion-card.component';
 import { NoSuggestionsComponent } from '@components/no-suggestions/no-suggestions.component';
-import { HomePageComponent } from '@components/HomePage/HomePage.component';
+// import { HomePageComponent } from '@components/HomePage/HomePage.component';
+// import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './pages/HomePage.component';
 @Component({
   standalone: true,
-  imports: [HomePageComponent, SuggestionCardComponent, NoSuggestionsComponent],
+  imports: [
+    // RouterOutlet,
+    HomePageComponent,
+    SuggestionCardComponent,
+    NoSuggestionsComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
